99f497d38bec75ad829caec533fd469c4f4d9575
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-07-27 04:23:17 +0600

    Fix: call send function

--- pages/api/safe-mint.ts
+++ pages/api/safe-mint.ts
@@ -25,15 +25,20 @@ export default async function handler(
   const kit = newKit(process.env.RPC_URL!);
   kit.defaultAccount = process.env.WALLET_ADDRESS;
   kit.connection.addAccount(process.env.RINKEBY_PRIVATE_KEY!);
+
   const token = new kit.web3.eth.Contract(
     // @ts-ignore
     SpaceTokenABI,
     process.env.CONTRACT_ADDRESS
   );
 
+  console.log({ acc: kit.defaultAccount });
+
   try {
-    const result = await token.methods.safeMint(address, name).call();
-    console.log({ result })
+    const result = await token.methods
+      .safeMint(address, name)
+      .send({ gas: 2100000, gasPrice: 200000000, from: kit.defaultAccount });
+    console.log({ result });
     res.status(200).json({ message: "Successfully minted" });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
