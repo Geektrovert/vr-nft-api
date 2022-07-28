731a326b812b53f9fa2b004bf099794afe8ab2aa
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-07-28 21:46:46 +0600

    Fix: web3

--- pages/api/safe-mint.ts
+++ pages/api/safe-mint.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { getWeb3WithAccount, getSpaceToken } from "../../utils/kitUtils";
+import { getWeb3WithAccount, getSpaceToken } from "../../utils/web3Utils";
 
 type Data = {
   message: string;
